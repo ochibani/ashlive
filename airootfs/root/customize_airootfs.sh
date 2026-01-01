2d4a0b6e8d451e958f6ab8989d8f00f7dd2d14d7
Chibani <85620152+ochibani@users.noreply.github.com>
2026-01-01 11:02:17 +0200

    Grant liveuser NOPASSWD access and enable services
    Added NOPASSWD access for liveuser and enabled display/network managers.

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -12,3 +12,9 @@ echo "liveuser:liveuser" | chpasswd
 # Enable the wheel group for sudo access
 echo "%wheel ALL=(ALL) ALL" | sudo tee /etc/sudoers.d/wheel
 sudo chmod 440 /etc/sudoers.d/wheel
+echo "liveuser ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/liveuser
+sudo chmod 440 /etc/sudoers.d/liveuser
+
+# Enable display manager & network manager
+systemctl enable sddm
+systemctl enable NetworkManager
