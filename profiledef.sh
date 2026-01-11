10d6a275ad7860636f68d1cb1aeff16f9a658272
Chibani <85620152+ochibani@users.noreply.github.com>
2026-01-11 10:15:43 +0200

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -18,6 +18,7 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:0400"
+  ["/etc/fonts/conf.d/99-arabic.conf"]="0:0:777"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
